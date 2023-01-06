2caaf3d777b46ac2bfcd58e96d892e63477630d6
Peter Marsh (He/Him) <pmarsh@scottlogic.com>
2023-01-06 12:15:43 +0000

    added method for random trades for testing

--- src/test/java/com/example/demo/TestUtils.java
+++ src/test/java/com/example/demo/TestUtils.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.matcher.models.Order;
 import com.example.demo.matcher.models.OrderAction;
 import com.example.demo.matcher.models.OrderbookItem;
+import com.example.demo.matcher.models.Trade;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -36,5 +37,16 @@ public class TestUtils {
 
     public static OrderbookItem makeOrderbookItem(float price, float quantity) {
         return new OrderbookItem(new BigDecimal(price), new BigDecimal(quantity));
+    }
+
+    public static Trade randomTrade() {
+        return new Trade(
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                new BigDecimal(Math.random()),
+                new BigDecimal(Math.random()),
+                LocalDateTime.now());
     }
 }
