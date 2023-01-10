47c63a7bc0e84327b89e31a38bb964a5db891e84
Peter Marsh (He/Him) <pmarsh@scottlogic.com>
2023-01-10 13:21:07 +0000

    configured controller unit test

---
+++ src/test/java/com/example/demo/MatcherControllerTest.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+import com.example.demo.matcher.Matcher;
+import com.example.demo.matcher.MatcherController;
+import com.example.demo.matcher.models.NewOrderParams;
+import com.example.demo.matcher.models.Order;
+import com.example.demo.matcher.services.OrderService;
+import com.example.demo.matcher.services.TradeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@WebMvcTest(controllers = MatcherController.class)
+public class MatcherControllerTest {
+
+    @Autowired
+    private MockMvc mvc; // model view container, which handles the web stuff
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private TradeService tradeService;
+
+    @MockBean
+    private Matcher matcher;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(mvc).isNotNull();
+    }
+
+    @Test
+    void ItShouldReturnReturnEmptyListAtFirstRequest() throws Exception {
+        MvcResult result = mvc.perform(
+                MockMvcRequestBuilders.get("/orderbook/buy"))
+                .andReturn();
+        assertThat(result.getResponse().getContentAsString()).isEqualTo("[]");
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
+
+    @Test
+    void ItShouldCreateAnOrder() throws Exception {
+        NewOrderParams newOrder = new NewOrderParams(
+                UUID.randomUUID().toString(),
+                1,
+                1,
+                "buy"
+        );
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/make/order")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(asJsonString(newOrder)))
+                .andReturn();
+        //assertThat(result.getResponse().getContentAsString()).isEqualTo("[]");
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
--- src/test/java/com/example/demo/MatcherTests.java
+++ src/test/java/com/example/demo/MatcherTests.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 public class MatcherTests {
     Matcher matcher;
     OrderService orderService;
