ad9496a6e8216e9f48cf7e43f6d51167ba667fa1
Peter Marsh (He/Him) <pmarsh@scottlogic.com>
2023-01-16 14:09:59 +0000

    added security config

---
+++ src/main/java/com/example/demo/security/configuration/ApplicationSecurity.java
@@ -0,0 +1,18 @@
+package com.example.demo.security.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
