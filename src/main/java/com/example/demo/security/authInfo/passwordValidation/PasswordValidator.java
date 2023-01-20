6df0fea01cec25c8e98655d21e4ed723935a7c64
Peter Marsh (He/Him) <pmarsh@scottlogic.com>
2023-01-20 16:15:10 +0000

    finished password vaidator

--- src/main/java/com/example/demo/security/authInfo/passwordValidation/PasswordValidator.java
+++ src/main/java/com/example/demo/security/authInfo/passwordValidation/PasswordValidator.java
@@ -3,7 +3,8 @@ package com.example.demo.security.authInfo.passwordValidation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraints.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordValidator implements ConstraintValidator<PasswordValidation, String> {
 
@@ -11,8 +12,8 @@ public class PasswordValidator implements ConstraintValidator<PasswordValidation
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-
-        return false;
+        Matcher matcher = pattern.matcher(value);
+        return matcher.matches();
     }
 }
 
